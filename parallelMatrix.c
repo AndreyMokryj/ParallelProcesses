5cddd31f7aced5dcc456859d4a56d6305502ed1f
andrew <andrew_mokryj@ukr.net>
2022-01-11 01:16:47 +0200

    refactor: improve performance

--- parallelMatrix.c
+++ parallelMatrix.c
@@ -1,11 +1,11 @@
-#include <mpi.h>    // import MPI header
-#include <stdio.h>  // needed for printf
-#include <math.h>   // needed for sqrt
+#include <mpi.h>
+#include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define SIZE 5000
+#define MATRIX_SIZE 1000
 
 
 int createRandomMatrix(int startPosition, int rows, int cols, double (*a))
@@ -22,89 +22,89 @@ int createRandomMatrix(int startPosition, int rows, int cols, double (*a))
 }
 
 int main(int argc, char *argv[]) {
-  srand(time(NULL));
-    printf("%ld", SIZE_MAX);
+    int totalNumber = MATRIX_SIZE * MATRIX_SIZE * 2;
+    srand(time(NULL));
     double *send;
-    send = (double*)malloc(SIZE * SIZE * 2 * sizeof(double));
+    send = (double*)malloc(totalNumber * sizeof(double));
     double *res;
-    res = (double*)malloc(SIZE * SIZE * sizeof(double));
+    res = (double*)malloc(totalNumber / 2 * sizeof(double));
   int rank, size, root;
-  MPI_Status status;
 
   MPI_Init(&argc, &argv); // initialize MPI
   MPI_Comm_rank(MPI_COMM_WORLD, &rank); // get own rank/ID
   MPI_Comm_size(MPI_COMM_WORLD, &size); // get total number of processes
 
   if(rank == 0) {
-    puts(" -- start creating 1st matrix");
-    createRandomMatrix(0, SIZE, SIZE, send);
-    puts(" -- start creating 2nd matrix");
-    createRandomMatrix(SIZE * SIZE, SIZE, SIZE, send);
+    printf("\nMatrix size: %d \n", MATRIX_SIZE);
+    createRandomMatrix(0, MATRIX_SIZE, MATRIX_SIZE, send);
+    createRandomMatrix(MATRIX_SIZE * MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, send);
   }
 
-    int number = SIZE * SIZE * 2;
-    MPI_Bcast(send, number, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    printf("Size of sent buffer: %d", number);
+    time_t startTime = time(NULL);
+    MPI_Bcast(send, totalNumber, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+//    printf("Size of sent buffer: %d", number);
     
     
   // each node now processes its share of the numbers
-    printf("\nProcess %d got following data\n", rank);
-    for(int i = 0; i < number; i++) {
-        printf("%-3.2f ", send[i]);
-    }
-    printf("\n");
+//    printf("\nProcess %d got following data\n", rank);
+//    for(int i = 0; i < number; i++) {
+//        printf("%-3.2f ", send[i]);
+//    }
+//    printf("\n");
     
-    double xRows = 1.0 * SIZE / size;
+    double xRows = 1.0 * MATRIX_SIZE / size;
     int startRow = floor(rank * xRows);
-    int endRow = SIZE;
+    int endRow = MATRIX_SIZE;
     if (rank < size - 1) {
         endRow = floor((rank + 1) * xRows);
     }
     
-    printf("Process %d start row %d end row %d", rank, startRow, endRow);
+//    printf("Process %d start row %d end row %d", rank, startRow, endRow);
     double *result;
-    result = (double*)malloc(number / 2 * sizeof(double));
+    result = (double*)malloc(totalNumber / 2 * sizeof(double));
     int rIndex = 0;
     for(int r = startRow; r < endRow; r++) {
-        for(int i = 0; i < SIZE; i++){
+        for(int i = 0; i < MATRIX_SIZE; i++){
             result[rIndex] = 0;
-            for(int j = 0; j < SIZE; j++){
-                result[rIndex] += send[r * SIZE + j] * send[SIZE * SIZE + SIZE * j + i];
+            for(int j = 0; j < MATRIX_SIZE; j++){
+                result[rIndex] += send[r * MATRIX_SIZE + j] * send[MATRIX_SIZE * MATRIX_SIZE + MATRIX_SIZE * j + i];
             }
             rIndex++;
         }
     }
         
-    printf("\nProcess %d got following results\n", rank);
-    for(int i = 0; i < rIndex; i++) {
-        printf("%-3.2f ", result[i]);
-    }
-    printf("\n");
+//    printf("\nProcess %d got following results\n", rank);
+//    for(int i = 0; i < rIndex; i++) {
+//        printf("%-3.2f ", result[i]);
+//    }
+//    printf("\n");
 
     int counts[size];
     int displacements[size];
     for(int i=0; i<size; i++){
         int startRow = floor(i * xRows);
-        int endRow = SIZE;
+        int endRow = MATRIX_SIZE;
         if (i < size - 1) {
             endRow = floor((i + 1) * xRows);
         }
-        counts[i] = SIZE * (endRow - startRow);
-        displacements[i] = SIZE * startRow;
+        counts[i] = MATRIX_SIZE * (endRow - startRow);
+        displacements[i] = MATRIX_SIZE * startRow;
     }
-    MPI_Gatherv(result, SIZE * (endRow - startRow), MPI_DOUBLE, res, counts, displacements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Gatherv(result, MATRIX_SIZE * (endRow - startRow), MPI_DOUBLE, res, counts, displacements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     if(rank == 0) { //if root, print
-        printf("\nResult: \n");
-        for (int i = 0; i < SIZE * SIZE; i++) {
-            printf("%-3.2f ", res[i]);
-        }
-        printf("\n");
+        time_t endTime = time(NULL);
+//        printf("\nResult: \n");
+//        for (int i = 0; i < SIZE * SIZE; i++) {
+//            printf("%-3.2f ", res[i]);
+//        }
+//        printf("\n");
+        free(send);
+        long long int st = (long long int)(startTime);
+        long long int et = (long long int)(endTime);
+        long long int diff = et - st;
+        printf("Time taken: %lld seconds\n", diff);
     }
 
-    if(rank == 0)
-    {
-      free(send);
-    }
     free(res);
     free(result);
     
